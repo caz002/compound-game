b80af2379f9a821bb52a4d840d2e7a7feb05f5e7
caz002 <caz002@ucsd.edu>
2025-07-26 09:00:12 -0700

    added main menu

---
+++ main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _ready():
+	pass
+func _process(delta):
+	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
