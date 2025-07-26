10a61d492e095164b01c6bc70c7f0a3b583dfd1a
caz002 <caz002@ucsd.edu>
2025-07-26 14:00:37 -0700

    added hover animation to main menu buttons

--- main_menu.gd
+++ main_menu.gd
@@ -1,10 +1,18 @@
 extends Control
 
+@export var tween_intensity: float
+@export var tween_duration: float
+
+@onready var start: Button = $VBoxContainer/Start_Game
+@onready var instructions: Button = $VBoxContainer/Instructions
+@onready var exit: Button = $VBoxContainer/Exit
+
 func _ready():
 	pass
-func _process(delta):
-	pass
-
+func _process(delta:float) -> void:
+	btn_hovered(start)
+	btn_hovered(instructions)
+	btn_hovered(exit)
 
 func _on_start_pressed():
 	$Click.play()
@@ -16,3 +24,14 @@ func _on_exit_pressed():
 	$Click.play()
 	await get_tree().create_timer(0.3).timeout
 	get_tree().quit()
+
+func start_tween(object: Object, property: String, final_val : Variant, duration: float):
+	var tween = create_tween()
+	tween.tween_property(object, property, final_val, duration)
+
+func btn_hovered(button: Button):
+	button.pivot_offset = button.size/2
+	if button.is_hovered():
+		start_tween(button, "scale", Vector2.ONE * tween_intensity, tween_duration)
+	else:
+		start_tween(button, "scale", Vector2.ONE, tween_duration)
